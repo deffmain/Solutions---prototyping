14e957e4767b3958c15392840a4d86fc42c7dd8b
Micael Martins <167147821+deffmain@users.noreply.github.com>
2026-06-11 17:22:38 -0300

    Update script.js

--- Dashboar/script.js
+++ Dashboar/script.js
@@ -1,1 +1,15 @@
+const repo = "deffmain/Solutions-portotyping";
 
+fetch(
+  `https://api.github.com/repos/${repo}/issues?state=all`
+)
+.then(response => response.json())
+.then(data => {
+
+  const abertas = data.filter(
+    issue => issue.state === "open"
+  ).length;
+
+  document.getElementById("abertas").innerText =
+    `Chamados abertos: ${abertas}`;
+});
